b19408074d469b68252c70e738ad68e6393489d3
Clare Hsu <clare.hsu390@gmail.com>
2018-03-27 16:49:47 -0700

    implement user model

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'rails', '~> 5.1.4'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
 gem 'pg_search'
+gem 'bcrypt'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, uniqueness: true
+    validates :password, length: { minimum: 6}, allow_nil: true
+
+    attr_reader :password
+    after_intialize :ensure_session_token
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by_username(username)
+        return nil unless user
+        user.is_password?(password) user : nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token ||= SecureRandom.base64
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.base64
+    end
+end
