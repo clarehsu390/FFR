1a884750058449a8f8e2e2c2014a60212b93cae4
Clare Hsu <clare.hsu390@gmail.com>
2018-03-27 16:39:25 -0700

    implement dog videos

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,2 +1,15 @@
 class Api::DogsController < ApplicationController
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :gender, :breed, :image_url, :description)
+    end
 end
