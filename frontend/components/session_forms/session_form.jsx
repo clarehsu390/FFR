0de83b1b6139b407326f4595d75f0c94e4568d46
Clare Hsu <clare.hsu390@gmail.com>
2018-03-28 13:08:39 -0700

    handle session form input

--- frontend/components/session_forms/session_form.jsx
+++ frontend/components/session_forms/session_form.jsx
@@ -1,1 +1,36 @@
 import React from 'react';
+
+export default class SessionForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+    }
+
+    handleChange(field) {
+        return e => this.setState({[field]: e.target.value});
+
+    }
+
+    handleSubmit() {
+
+    }
+
+    render() {
+        if (!this.props.loggedIn) {
+
+        return (<form>
+            <label>Username:
+                <input type="text" value={this.state.username} onChange={this.handleChange('username')}/>
+                </label>
+            <label>Password:
+                <input type="password" value={this.state.password} onChange={this.handleChange('password')}/>
+                </label>
+                <input type="submit"/>
+            </form>
+            );
+        }
+    }
+}
