46a26df7383cba29f8c3e8c918ee85885be5ebc2
Clare Hsu <clare.hsu390@gmail.com>
2018-03-27 19:13:48 -0700

    include session reducer

--- frontend/ffr.jsx
+++ frontend/ffr.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as APIUtil from './util/session_api_util';
+import { signup, login, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Furry Friends Rescue</h1>, root);
-    window.login = APIUtil.login;
+    window.login = login;
 });
